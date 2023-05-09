5594db2023a60741d8ae070587d622b00ec2566e
Sunny <contact.kumarsunny@gmail.com>
2023-05-09 15:40:27 +0530

    added MUltiply.java

---
+++ Multiply.java
@@ -0,0 +1,11 @@
+public class Multiply {
+
+    public static void main(String[] args) {
+        int a=4;
+        int b=3;
+
+        System.out.println("The product is: "+ (a*b));
+        
+    }
+    
+}
